016e1aa74e3e4f2bac27d0cc2d878f8c36c89b4b
satwik1998 <satwikgawas1998@gmail.com>
2026-01-08 11:29:57 +0530

    logo cahnages

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
       <section className="px-16 py-10 border-t border-gray-700">
         <h2 className="text-2xl font-bold mb-4">About Me</h2>
         <p className="text-gray-400 max-w-3xl">
-          I am a passinote a full stack developer with experience in building
+          I am a passinate a full stack developer with experience in building
           web applications using the latest technologies. I love solving
           problems and creative innovative solutions
         </p>
